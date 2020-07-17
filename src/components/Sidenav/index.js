efca62e749f804aaf275e39cc424502607954051
ivb98 <ivanvalette@hotmail.com>
2020-07-16 22:14:20 -0400

    feat: Add hover style to sidenav

--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidenav = ({ isOpen, className }) => {
+    const routes = [
+        {
+            name: 'Recent',
+            link: '/',
+        },
+        {
+            name: 'Subscriptions',
+            link: '/',
+        },
+        {
+            name: 'Liked Videos',
+            link: '/',
+        },
+    ];
     return (
         <nav
             className={`transform mt-8 pt-2 ${className} ${
                 isOpen ? 'translate-x-0' : '-translate-x-full'
             } md:translate-x-0 fixed md:fixed h-full w-5/12 md:w-2/12 bg-white transition duration-500 transition-transform z-10`}
         >
-            <ul className="pl-6 text-sm">
-                <li className="pt-2 cursor-pointer">Recent</li>
-                <li className="pt-2 cursor-pointer">Subscriptions</li>
-                <li className="pt-2 cursor-pointer">Liked Videos</li>
+            <ul className="text-sm">
+                {routes.map((route, i) => (
+                    <li className="py-2 hover:bg-gray-200" key={i}>
+                        <Link className="pl-6 block" to={route.link}>
+                            {route.name}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </nav>
     );
