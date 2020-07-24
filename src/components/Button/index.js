340146d97d85794b9d9a159d61aca4a653682f70
ivb98 <ivanvalette@hotmail.com>
2020-07-24 00:47:30 -0400

    Add focus and hover state to button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ children, className, ...rest }) => {
     return (
         <button
-            className={`bg-red-600 py-2 px-3 font-bold text-white rounded w-full ${className}`}
+            className={`bg-red-600 hover:bg-red-700 focus:bg-red-700 py-2 px-3 font-bold text-white rounded w-full ${className}`}
             {...rest}
         >
             {children}
