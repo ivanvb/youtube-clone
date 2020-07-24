0508f5bda62fae5d3903e4bb2af9294ea2befca6
ivb98 <ivanvalette@hotmail.com>
2020-07-24 00:46:29 -0400

    Add login form functionality

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,15 +4,45 @@ import InputField from '../../components/InputField/index';
 import Button from '../../components/Button/index';
 import LobbyContainer from '../../components/LobbyContainer/index';
 import { Link } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
+import useFetch from '../../hooks/useFetch';
 
 const LoginForm = () => {
+    const [form, setForm] = useForm({ email: '', password: '' });
+    const [data, loading, error, util] = useFetch('/api/login/');
+    function handleSubmit(e) {
+        e.preventDefault();
+        util.start({
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(form),
+        });
+    }
     return (
         <>
             <Heading1 className="text-gray-800 mb-20">Youtube Clone</Heading1>
             <p className="mb-4">Login with your existing account</p>
-            <InputField type="text" placeholder="Email" className="mb-4" />
-            <InputField type="password" placeholder="Password" className="mb-4" />
-            <Button>Login</Button>
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <InputField
+                    required={true}
+                    name="email"
+                    value={form.email}
+                    onChange={(e) => setForm(e)}
+                    type="email"
+                    placeholder="Email"
+                    className="mb-4"
+                />
+                <InputField
+                    required={true}
+                    name="password"
+                    value={form.password}
+                    onChange={(e) => setForm(e)}
+                    type="password"
+                    placeholder="Password"
+                    className="mb-4"
+                />
+                <Button>Login</Button>
+            </form>
             <p className="absolute bottom-0 mb-6">
                 Don't have an account?{' '}
                 <Link to="/signup" className="font-bold">
